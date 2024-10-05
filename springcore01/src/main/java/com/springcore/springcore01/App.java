e6ecaa572282ac1879193842be8bdf7f2a620b96
PURVA ATHNERE <93878803+purvathnere@users.noreply.github.com>
2024-10-05 23:05:14 +0530

    Add files via upload

---
+++ springcore01/src/main/java/com/springcore/springcore01/App.java
@@ -0,0 +1,23 @@
+package com.springcore.springcore01;
+
+
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class App {
+    public static void main(String[] args) {
+    	System.out.println("Hello,welcome");
+        ApplicationContext context = new ClassPathXmlApplicationContext("config.xml");
+        
+        Student student1 = (Student) context.getBean("student1");
+        System.out.println(student1);
+        
+        Student student2 = (Student) context.getBean("student2");
+        System.out.println(student2);
+        
+        Student student3 = (Student) context.getBean("student3");
+        System.out.println(student3);
+    }
+}
+
